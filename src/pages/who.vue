462dd568327c1f342646bf56332d9824b7ab79e1
Mathew Button <mathewdbutton@gmail.com>
2020-01-17 20:41:20 +1100

    Add about details

---
+++ src/pages/who.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <h1>Who?</h1>
+    <p>Hi all, I'm Mat. I'm a software developer currently based in Sydney, Australia (not Sydney, Nova Scotia), this blog is an attempt to motivate
+      myself to actually finish the random projects I set out to do, hopefully with somewhere to showcase it I won't just float from one project to the next.
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Who?'
+  }
+}
+</script>
